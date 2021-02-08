91c13b0911007e1539a1d9d33656e4b82d230add
dabryant11 <74263550+dabryant11@users.noreply.github.com>
2021-02-08 13:36:28 -0500

    post-lunch

--- src/index.js
+++ src/index.js
@@ -22,16 +22,30 @@ function getToyObjects() {
     headers: {
       "Content-Type": "application/json"
     }
+    // data = JSON.parse(data)
+
+    // var data = JSON.stringify([{"model": "app.mdl", "pk": 1, "fields": {"name": "test", "rank": 1}}]);
+    // data.forEach(function(element){
+    //     console.log(element);
+    // });
+
+
+
   }).then(response => { return response.json()
   }).then(response => allToys(response))
+
 }
 
-getToyObjects()
-console.log(getToyObjects())
+// getToyObjects()
+// console.log(getToyObjects())
+const newToyBtn = document.querySelector('#new-toy-btn')
+newToyBtn.addEventListener('click', allToys)
 
-function allToys(data){
+function allToys(response){
+  console.log(response)
+  console.log(JSON.stringify(response));
 
-  data.forEach((item, index) => {
+  response.forEach((item, index) => {
     // 1 create tags 
     // 2 append in order we want them 
     // 3 fill them with what we want to fill them with 
@@ -54,10 +68,10 @@ function allToys(data){
 
     toyCollection.appendChild(newDiv)
 
-    name.innerHTML = item.name 
-    image.innerHTML = item.image
-    likes.innerHTML = `${item.likes} + " likes"`
-    likeBtn.innerHTML = "like"
+    // name.innerHTML = item.name 
+    // image.innerHTML = item.image
+    // likes.innerHTML = `${item.likes} + " likes"`
+    // likeBtn.innerHTML = "like"
 
     // likes.textContent = contentArray[index].likes 
     // if (contentArray[index].likes > 1 || contentArray[index].likes === 0) {
