b5226cc5c08ceff840f613739cb4cf0deb45f833
dabryant11 <74263550+dabryant11@users.noreply.github.com>
2021-02-08 12:02:44 -0500

    lunch break

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let addToy = false;
+let toyCollection = document.querySelector('#toy-collection')
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -13,3 +14,56 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function getToyObjects() {
+  const url = "http://localhost:3000/toys";
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(response => { return response.json()
+  }).then(response => allToys(response))
+}
+
+getToyObjects()
+console.log(getToyObjects())
+
+function allToys(data){
+
+  data.forEach((item, index) => {
+    // 1 create tags 
+    // 2 append in order we want them 
+    // 3 fill them with what we want to fill them with 
+    const newDiv = document.createElement('div');
+    // const id = document.createElement('h2');
+    const name = document.createElement('h2');
+    // name.className = 'name'
+    // name.className = 'name'
+    const image = document.createElement('img');
+    img.className = 'toy-avatar'
+    const likes = document.createElement('p');
+    // likes.className = 'likes'
+    const likeBtn = document.createElement('button');
+    likeBtn.className = 'like-btn'
+
+    newDiv.appendChild(id)
+    newDiv.appendChild(name)
+    newDiv.appendChild(image)
+    newDiv.appendChild(likes)
+
+    toyCollection.appendChild(newDiv)
+
+    name.innerHTML = item.name 
+    image.innerHTML = item.image
+    likes.innerHTML = `${item.likes} + " likes"`
+    likeBtn.innerHTML = "like"
+
+    // likes.textContent = contentArray[index].likes 
+    // if (contentArray[index].likes > 1 || contentArray[index].likes === 0) {
+    //     likes.textContent += ' likes.'
+    // } else {
+    //     likes.textContent += ' like.'
+    // }
+  })
+}
